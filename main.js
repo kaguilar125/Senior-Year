bc91ca946c211a67ce3605886f14596d99a77902
Kevin <125kaguilarmartinez@frhsd.com>
2024-09-18 12:54:19 -0400

    ufhafh

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 var swiper = new Swiper(".home", {
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
